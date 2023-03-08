e8b761bae94fad7a61d33f5abeb3017dfea9d1d8
Chris Kim <chriskim06@gmail.com>
2023-03-08 12:14:03 -0800

    lint

--- app/commands.ts
+++ app/commands.ts
@@ -1,7 +1,7 @@
-import { app, Menu, BrowserWindow } from 'electron';
-import { openConfig, getConfig } from './config';
-import { updatePlugins } from './plugins';
-import { installCLI } from './utils/cli-install';
+import {app, Menu, BrowserWindow} from 'electron';
+import {openConfig, getConfig} from './config';
+import {updatePlugins} from './plugins';
+import {installCLI} from './utils/cli-install';
 import * as systemContextMenu from './utils/system-context-menu';
 
 const commands: Record<string, (focusedWindow?: BrowserWindow) => void> = {
@@ -51,7 +51,7 @@ const commands: Record<string, (focusedWindow?: BrowserWindow) => void> = {
     if (webContents.isDevToolsOpened()) {
       webContents.closeDevTools();
     } else {
-      webContents.openDevTools({ mode: 'detach' });
+      webContents.openDevTools({mode: 'detach'});
     }
   },
   'zoom:reset': (focusedWindow) => {
@@ -131,7 +131,7 @@ const commands: Record<string, (focusedWindow?: BrowserWindow) => void> = {
   },
   'window:hamburgerMenu': () => {
     if (process.platform !== 'darwin' && ['', true].includes(getConfig().showHamburgerMenu)) {
-      Menu.getApplicationMenu()!.popup({ x: 25, y: 22 });
+      Menu.getApplicationMenu()!.popup({x: 25, y: 22});
     }
   },
   'systemContextMenu:add': () => {
